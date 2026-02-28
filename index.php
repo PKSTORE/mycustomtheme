a2665e5bc5b07f1d54bd31f70616fd9806faec59
Piyush Sharma <sharmapiyush1342@gmail.com>
2026-02-28 16:00:43 +0530

    added full html desgin to index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
       <!-- Logo -->
       <div class="logo">
-        <a href="index.html">
+       <a href="<?php echo site_url('/'); ?>">Home</a>
    <img src="<?php echo get_template_directory_uri(); ?>/img/RRC-Long-logo.png">
         </a>
 
