871bd87042f2dda0c03a4548e7042fa6e197349a
Piyush Sharma <sharmapiyush1342@gmail.com>
2026-03-05 17:07:27 +0530

    added html file to index.php

--- index.php
+++ index.php
@@ -91,8 +91,7 @@
         <h3>Enquire Now</h3>
         <p>Enter your contact details to schedule a repair.</p>
 
-        <form id="enquiryForm">
-
+        <form  class="hero-2-form"git  id="enquiryForm">
 <div class="input-box">
 <input type="text" name="client_name" placeholder="Your name" required>
 </div>
