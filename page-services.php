948ff8bf0c30098f59e02ea785c469c85ace2cd5
Piyush Sharma <sharmapiyush1342@gmail.com>
2026-03-05 14:19:22 +0530

    added full html to page-services.php

--- page-services.php
+++ page-services.php
@@ -179,4 +179,10 @@
       © 2026 RR COMPUTERS.
     </div>
   </footer>
+  <!-- script 1 -->
+    <script>
+    function toggleMenu() {
+      document.getElementById("navMenu").classList.toggle("show");
+    }
+  </script>
 <?php get_footer(); ?>
